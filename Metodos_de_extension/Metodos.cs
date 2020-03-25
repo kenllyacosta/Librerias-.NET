875f86d70a82e918aae404116909dca03a02f043
Kenlly Acosta <kenllyacosta@hotmail.com>
2020-03-25 18:09:59 -0400

    Add project files.

---
+++ Metodos_de_extension/Metodos.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Metodos_de_extension
+{
+    public static class Metodos
+    {
+        /// <summary>
+        /// Retorna todo el string consultado
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="enlace">
+        /// Enlace de la consulta a la API
+        /// </param>
+        /// <returns></returns>
+        public static string ConsultaApi(this object obj, string enlace)
+        {
+            string resultado = default;
+
+            HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create(enlace.Trim());
+
+            try
+            {
+                var response = myReq.GetResponse();
+
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                    resultado = reader.ReadToEnd();
+                }
+            }
+            catch { }
+
+            return resultado;
+        }
+
+        /// <summary>
+        /// Retorna una lista del tipo a consultar.
+        /// Referencia: http://json2csharp.com/
+        /// Convierte un objeto JSon a una clase de C#
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="enlace"></param>
+        /// <returns></returns>
+        public static TEntity ConsultaApi<TEntity>(this object obj, string enlace)
+        {
+            TEntity resultado = default;
+
+            HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create(enlace.Trim());
+
+            try
+            {
+                var response = myReq.GetResponse();
+
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                    resultado = JsonConvert.DeserializeObject<TEntity>(reader.ReadToEnd());
+                }
+            }
+            catch { }
+
+            return resultado;
+        }
+    }
+}
---
+++ Pruebas/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Metodos_de_extension;
+
+namespace Pruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MetodosDeExtension(args);
+            Console.WriteLine("Hola mundo!");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Prueba de los métodos de extensión
+        /// </summary>
+        /// <param name="args">
+        /// Objeto para usar con cualquier método creado en el proyecto Metodos_de_extension
+        /// </param>
+        private static void MetodosDeExtension(string[] args)
+        {
+            var datos = args.ConsultaApi("https://ticapacitacion.com/cats");
+            var datosDeGatos = args.ConsultaApi<List<Cat>>("https://ticapacitacion.com/cats");
+        }
+    }
+
+    public class Cat
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string WebSite { get; set; }
+        public string Image { get; set; }
+    }
+}
